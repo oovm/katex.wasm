9c5031d791e50c75518010bb426b5bc2e13343a0
Aster <galaster@foxmail.com>
2020-10-21 20:40:23 +0800

    🔮 Use local storage to restore the last closed state

--- projects/yew-katex-preview/src/main.rs
+++ projects/yew-katex-preview/src/main.rs
@@ -1,6 +1,12 @@
 #![recursion_limit = "1024"]
 
-use yew::{html, prelude::*, Component, ComponentLink, Html, ShouldRender};
+use yew::{
+    format::Json,
+    html,
+    prelude::*,
+    services::storage::{Area, StorageService},
+    Component, ComponentLink, Html, ShouldRender,
+};
 use yew_katex::KaTeX;
 
 pub fn header_view() -> Html {
@@ -8,7 +14,7 @@ pub fn header_view() -> Html {
     html! {
     <header>
         <h1 color="#009688">{title}</h1>
-        <a href="https://github.com/GalAster/note-to-yew">{"Fork me!"}</a>
+        <a href="https://github.com/GalAster/yew-katex">{"Fork me!"}</a>
     </header>
     }
 }
@@ -19,6 +25,7 @@ pub enum Event {
 
 pub struct Model {
     link: ComponentLink<Self>,
+    storage: StorageService,
     input: String,
 }
 
@@ -27,15 +34,24 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self {
-            link,
-            input: String::from(r#"\int_{\partial M}^{}\omega＝\int_{M}^{}\mathrm{d}\,\omega"#),
-        }
+        let storage = StorageService::new(Area::Local).expect("storage was unavailable!");
+        let input = {
+            match storage.restore("tex") {
+                Json(Ok(restored_model)) => restored_model,
+                _ => {
+                    String::from(r#"\int_{\partial M}^{}\omega＝\int_{M}^{}\mathrm{d}\,\omega"#)
+                }
+            }
+        };
+        Self { link, storage, input }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Event::Input(s) => self.input = s,
+            Event::Input(s) => {
+                self.input = s;
+                self.storage.store("tex", Json(&self.input))
+            }
         }
         true
     }
